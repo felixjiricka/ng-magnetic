5f7c86707e1ba3f6eca279e372b2687dccc873b1
felixjiricka <felixjiricka@outlook.com>
2021-12-26 00:35:20 +0100

    add built demo app for github demo

--- projects/ng-magnetic-app/src/app/app.module.ts
+++ projects/ng-magnetic-app/src/app/app.module.ts
@@ -2,8 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NgMagneticModule } from 'ng-magnetic';
-import { NgCursorModule } from 'projects/ng-magnetic/src/public-api';
+import { NgMagneticModule, NgCursorModule } from 'ng-magnetic';
 
 @NgModule({
   declarations: [
--- projects/ng-magnetic/src/public-api.ts
+++ projects/ng-magnetic/src/public-api.ts
@@ -1,10 +1,8 @@
 /*
  * Public API Surface of ng-magnetic
  */
-
-export * from './lib/directives/ng-magnetic/ng-magnetic.directive';
-export * from './lib/directives/ng-magnetic/ng-magnetic.module';
-
-export * from './lib/components/ng-cursor/ng-cursor.component';
 export * from './lib/components/ng-cursor/ng-cursor.module';
+export * from './lib/components/ng-cursor/ng-cursor.component';
 
+export * from './lib/directives/ng-magnetic/ng-magnetic.module';
+export * from './lib/directives/ng-magnetic/ng-magnetic.directive';
